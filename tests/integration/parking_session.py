422cdba8831d2759e01b44a9a1598fadcce0b28c
bryandevuono <bryandevuono@gmail.com>
2025-10-13 15:39:24 +0200

    added hint typing

--- tests/integration/parking_session.py
+++
@@ -1,3 +0,0 @@
-import requests
-
-BASE_URL = "http://localhost:8000"
--- tests/integration/reservations.py
+++ tests/integration/reservations.py
@@ -1,3 +1,15 @@
 import requests
 
 BASE_URL = "http://localhost:8000"
+
+# happy flow
+def test_get_all_reservations(headers):
+    response = requests.get(f"{BASE_URL}/reservations", headers=headers)
+    assert response.status_code == 200
+    
+# sad flow
+def test_get_non_existing_reservation(headers):
+    response = requests.get(f"{BASE_URL}/reservations", headers=headers)
+    assert response.status_code == 404
+    
+    
