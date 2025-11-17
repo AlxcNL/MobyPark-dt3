652734d62688a8d2cae9f7025970a59f94a480fa
Julina <Julinamay_@hotmail.com>
2025-11-17 11:59:16 +0100

    Add parking-lot into main and in models

--- v2/app/main.py
+++ v2/app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from app.endpoints import oauth
+from app.endpoints import parking_lots
 
 app = FastAPI(title="MobyPark API v2")
 app.include_router(oauth.router)
+app.include_router(parking_lots.router)
 
 @app.get("/")
 async def root():
--- v2/app/models.py
+++ v2/app/models.py
@@ -32,8 +32,27 @@ class User(Base):
         CheckConstraint("role in ('user','admin')", name="ck_users_role"),
     )
 
-class ParkingLot():
-    pass
+class ParkingLot(Base):
+    __tablename__ = "parking_lots"
+    
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    location: Mapped[Optional[str]] = mapped_column(String, nullable=True)
+    address: Mapped[Optional[str]] = mapped_column(String, nullable=True)
+    capacity: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    reserved: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    tariff: Mapped[Optional[float]] = mapped_column(String, nullable=True)
+    daytariff: Mapped[Optional[float]] = mapped_column(String, nullable=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+    latitude: Mapped[Optional[float]] = mapped_column(Float, nullable=True)
+    longitude: Mapped[Optional[float]] = mapped_column(Float, nullable=True)
+
+    table_args = (
+        CheckConstraint("capacity >= 0", name="ck_parking_lots_capacity"),
+        CheckConstraint(
+            "reserved >= 0 AND reserved <= capacity", name="ck_parking_lots_reserved"
+        ),
+    )
 
 class Payment():
     pass
