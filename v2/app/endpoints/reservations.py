4f787a400c0cbf5f557d7f02a424e67220d8026f
bryandevuono <bryandevuono@gmail.com>
2025-11-19 17:06:20 +0100

    added create endpoint

---
+++ v2/app/endpoints/reservations.py
@@ -0,0 +1,48 @@
+from typing import List
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, func
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
+from app.database import get_db
+from app import models, schemas
+from app.security import check_token ,require_admin
+from app.dependancies import get_current_user, page_params, PageParams
+
+router = APIRouter(prefix="", tags=["reservations"])
+bearer_scheme = HTTPBearer(auto_error=True)
+@router.post("/reservations", response_model=schemas.Reservation)
+async def create_reservation(
+    reservation: schemas.ReservationCreate,
+    db: AsyncSession = Depends(get_db),
+    current_user: models.User = Depends(get_current_user),
+    token: HTTPAuthorizationCredentials = Depends(bearer_scheme)
+):
+    check_token(token.credentials)
+
+    result = await db.execute(
+        select(models.Vehicle).where(
+            models.Vehicle.id == reservation.vehicles_id,
+            models.Vehicle.users_id == current_user.id
+        )
+    )
+    vehicle = result.scalar_one_or_none()
+    if not vehicle:
+        raise HTTPException(
+            status_code=403,
+            detail="Vehicle does not exist or does not belong to the user"
+        )
+    new_reservation = models.Reservation(
+        vehicles_id=reservation.vehicles_id,
+        parking_lots_id=reservation.parking_lots_id,
+        start_time=reservation.start_time,
+        end_time=reservation.end_time,
+        status="confirmed",
+        cost=reservation.cost
+    )
+    
+    db.add(new_reservation)
+    await db.commit()
+    await db.refresh(new_reservation)
+    
+    return new_reservation
