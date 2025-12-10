48c3c585f6c170f34cd62e09d8b74ea043d27237
Darren Siriram <ds.siriram@gmail.com>
2025-12-10 20:51:21 +0100

    Implement logging setup and configure ELK stack integration

--- v2/app/main.py
+++ v2/app/main.py
@@ -1,22 +1,10 @@
 import logging
-import sys
-from fastapi import FastAPI
-from ecs_logging import StdlibFormatter
 
+from fastapi import FastAPI
+from app.logging_setup import setup_logging 
 from app.endpoints import oauth, vehicles, parking_lots, reservations, sessions, payments, billing
 
-# Setup logging voor ELK-stack.
-def setup_logging():
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    if logger.hasHandlers():
-        logger.handlers.clear()
-    handler = logging.StreamHandler(sys.stdout)
-    formatter = StdlibFormatter()
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
 setup_logging()
-
 app = FastAPI(title="MobyPark API v2")
 app.include_router(oauth.router)
 app.include_router(vehicles.router)
@@ -28,5 +16,5 @@ app.include_router(billing.router)
 
 @app.get("/")
 async def root():
-    logging.info("Dummy LOG ASAH")
+    logging.info("Root endpoint accessed")
     return {"message": "Welcome to MobyPark API v2"}
